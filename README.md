a60698d1a51adc08b20c00eba5f32076e4bb3da9
ZhexiongLiu <macodreu@gmail.com>
2023-05-17 18:33:04 +0800

    upload code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ImageArg-Shared-Task
+# ImageArg-Shared-Task-2023
+
+## Information
+Webpage: [ImageArg Shared Task](https://imagearg.github.io/)
+
+Join Slack: [ImageArg Slack](https://join.slack.com/t/imagearg/shared_invite/zt-1ss5hdb6d-eNCaWOAEe4O_8UE1gQxIxA)
+
+Signup Form: [Google Form](https://docs.google.com/forms/d/e/1FAIpQLSci3TSw6ylcWnjXQsoUjh3buAQx7IdgiJwrJDR2pDHMm8DIpQ/viewform)
+
+## Introduction
+There has been a recent surge of interest in developing methods and corpora to improve and evaluate persuasiveness in natural language applications. However, these efforts have mainly focused on the textual modality, neglecting the influence of other modalities. [Liu et al.](https://aclanthology.org/2022.argmining-1.1.pdf) introduced a new multimodal dataset called “ImageArg” This dataset includes persuasive tweets along with associated images, aiming to identify the image's stance towards the tweet and determine its persuasiveness score concerning a specific topic. The ImageArg dataset is a significant step towards advancing multimodal persuasive text analysis and opens up avenues for exploring the persuasive impact of images in social media. To further this goal, we designed this shared task, which utilizes the ImageArg dataset to advance multimodal persuasiveness techniques.
+
+## Dataset
+Please note that the downloaded dataset should only be used for participating in ImageArg Shared Task. Any other use is explicitly prohibited. Any participants are not allowed to redistribute the dataset per Twitter Developer Policy: https://developer.twitter.com/en/developer-terms/policy.
+
+All the tweets are instantly crawled from Twitter. We are aware that some tweets could not be available when participants start to download (e.g., a tweet is deleted by its author). We will regularly monitor the whole dataset and provide data patches to replace invalid tweets. Therefore, it is required to fill out the [Google Form](https://docs.google.com/forms/d/e/1FAIpQLSci3TSw6ylcWnjXQsoUjh3buAQx7IdgiJwrJDR2pDHMm8DIpQ/viewform) in order to receive data patches and to participate in the shared task.
+
+Please run `python get_tweet_data.py`
+
+## Citation
+``````
+@inproceedings{liu-etal-2022-imagearg,
+    title = "{I}mage{A}rg: A Multi-modal Tweet Dataset for Image Persuasiveness Mining",
+    author = "Liu, Zhexiong  and Guo, Meiqi  and Dai, Yue  and Litman, Diane",
+    booktitle = "Proceedings of the 9th Workshop on Argument Mining",
+    month = oct,
+    year = "2022",
+    address = "Online and in Gyeongju, Republic of Korea",
+    publisher = "International Conference on Computational Linguistics",
+    url = "https://aclanthology.org/2022.argmining-1.1",
+    pages = "1--18"
+    }
+``````
+
+## Organizer
+
+Zheixong Liu, Mohamed Elaraby, Yang Zhong, and Diane Litman (University of Pittsburgh)
---
+++ get_tweet_data.py
@@ -0,0 +1,112 @@
+"""
+=========================================================
+Title: ImageArg Shared Task Code - Dataset Downloader
+---------------------------------------------------------
+Warning: The downloaded dataset should only be used for
+participating in ImageArg Shared Task. Any other use is
+explicitly prohibited. Any participants are not allowed
+to redistribute the dataset per Twitter Developer Policy:
+https://developer.twitter.com/en/developer-terms/policy.
+---------------------------------------------------------
+Notice: This code is managed by ImageArg Shared Task
+(https://imagearg.github.io/). Parts of the code are
+adopted from https://github.com/markowanga/stweet.
+---------------------------------------------------------
+Data: 2023-05-15
+=========================================================
+"""
+
+from tqdm import tqdm
+import pandas as pd
+import stweet as st
+import requests
+import argparse
+import json
+import os
+
+
+def try_tweet_by_id_scrap(tweet_id):
+    id_task = st.TweetsByIdTask(tweet_id)
+    output_print = st.PrintRawOutput()
+    output_collector = st.CollectorRawOutput()
+    st.TweetsByIdRunner(tweets_by_id_task=id_task,
+                        raw_data_outputs=[output_print, output_collector]).run()
+    return output_collector
+
+
+def run(topic="abortion"):
+    if not os.path.exists(args.data_dir):
+        os.mkdir(args.data_dir)
+    if not os.path.exists(os.path.join(args.data_dir, "images")):
+        os.mkdir(os.path.join(args.data_dir, "images"))
+    if not os.path.exists(os.path.join(args.data_dir, "images", f"{topic}")):
+        os.mkdir(os.path.join(args.data_dir, "images", f"{topic}"))
+
+    # if not os.path.exists(args.meta_data):
+    #     raise "No meta data found! Please download meta data..."
+    # with open(args.meta_data) as f:
+    #     lines = json.load(f)
+
+    headers = {'Accept': 'application/json'}
+    lines = requests.get(args.meta_data, headers=headers).json()
+    lines = [line for line in lines if line["topic"] == topic]
+
+    tweetid_list = []
+    tweeturl_list = []
+    tweettext_list = []
+    stance_list = []
+    persuasiveness_list = []
+    split_list = []
+    for item in tqdm(lines):
+        tweetid = item['tweet_id']
+        stance = item["stance"]
+        persuasiveness = item["persuasiveness"]
+        tweeturl = item["tweet_url"]
+        split = item["split"]
+
+        # save time in case the images are downloaded
+        if os.path.exists(os.path.join(args.data_dir, "images", f"{topic}", f"{tweetid}.jpg")):
+            continue
+
+        try:
+            output_collector = try_tweet_by_id_scrap(item['tweet_id'])
+            jsonline = json.loads(output_collector.get_raw_list()[0].to_json_line())
+            text = jsonline['raw_value']['legacy']['full_text']
+
+            img_url = jsonline['raw_value']['legacy']['entities']['media'][0]['media_url_https']
+            img_data = requests.get(img_url, timeout=10).content
+            with open(os.path.join(args.data_dir, "images", f"{topic}", f"{tweetid}.jpg"), 'wb') as handler:
+                handler.write(img_data)
+
+            tweettext_list.append(text)
+            tweetid_list.append(tweetid)
+            tweeturl_list.append(tweeturl)
+            stance_list.append(stance)
+            persuasiveness_list.append(persuasiveness)
+            split_list.append(split)
+
+            df = pd.DataFrame()
+            df['tweet_id'] = tweetid_list
+            df["tweet_url"] = tweeturl_list
+            df['tweet_text'] = tweettext_list
+            df['stance'] = stance_list
+            df["persuasiveness"] = persuasiveness_list
+            df["split"] = split_list
+
+            df_train = df[df["split"] == "train"]
+            df_dev = df[df["split"] == "dev"]
+            df_train.to_csv(os.path.join(f"{args.data_dir}", f"{topic}_train.csv"), index=False)
+            df_dev.to_csv(os.path.join(f"{args.data_dir}", f"{topic}_dev.csv"), index=False)
+
+        except Exception as e:
+            print("Skip Error", e)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Persuasiveness')
+    parser.add_argument('--meta-data', default='https://people.cs.pitt.edu/~zhexiong/data/meta_data.json', help='meta data path')
+    parser.add_argument('--data-dir', default='./data', help='path to save data')
+    args = parser.parse_args()
+    for topic in ["gun_control", "abortion"]:
+        run(topic=topic)
+    print("Done!")
