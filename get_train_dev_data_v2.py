7927f9886173cb8cbe271da3f714fa33cfeb025d
ZhexiongLiu <macodreu@gmail.com>
2023-07-03 19:36:49 -0700

    update code

---
+++ get_train_dev_data_v2.py
@@ -0,0 +1,22 @@
+"""
+=========================================================
+Title: ImageArg Shared Task Code - Dataset Downloader
+---------------------------------------------------------
+Warning: The downloaded dataset should only be used for
+participating in ImageArg Shared Task. Any other use is
+explicitly prohibited. Any participants are not allowed
+to redistribute the dataset per Twitter Developer Policy:
+https://developer.twitter.com/en/developer-terms/policy.
+---------------------------------------------------------
+Notice: This code is managed by ImageArg Shared Task
+(https://imagearg.github.io/). # please keep dataset.so
+in the root folder in order to run the code.
+---------------------------------------------------------
+Data: 2023-07-03
+=========================================================
+"""
+
+from dataset import *
+if __name__ == '__main__':
+    get_train_dev_data()
+
